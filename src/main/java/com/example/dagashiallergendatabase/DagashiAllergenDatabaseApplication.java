baaeaf4a1575963ce851d43d285e0f4796836c11
Naoya Kamegaya <kaameesaan@gmail.com>
2023-03-22 14:15:15 +0900

    プロジェクトの作成

---
+++ src/main/java/com/example/dagashiallergendatabase/DagashiAllergenDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.dagashiallergendatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DagashiAllergenDatabaseApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DagashiAllergenDatabaseApplication.class, args);
+  }
+
+}
