96c79497dc197b94b4cc6023a2c51957e449f042
naoya-kamegaya <kaameesaan@gmail.com>
2023-07-15 20:54:06 +0900

    Createの実装に向けMapperにINSERT文を追加

--- src/main/java/com/example/dagashiallergendatabase/mapper/SnackMapper.java
+++ src/main/java/com/example/dagashiallergendatabase/mapper/SnackMapper.java
@@ -1,6 +1,7 @@
 package com.example.dagashiallergendatabase.mapper;
 
 import com.example.dagashiallergendatabase.entity.Snack;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -12,7 +13,20 @@ public interface SnackMapper {
   @Select("SELECT * FROM snacks")
   List<Snack> findAll();
 
-  
   @Select("SELECT * FROM snacks WHERE id = #{id}")
   Optional<Snack> findById(Integer id);
+
+  @Insert("INSERT INTO snacks(id, name, containsCrabs, containsWheat, containsBuckwheat, containsEggs, containsMilk, containsPeanuts, updatedAt) VALUES (#{id}, #{name}, #{containsCrabs}, #{containsWheat}, #{containsBuckwheat}, #{containsEggs}, #{containsMilk}, #{containsPeanuts}, #{updatedAt})")
+  void createSnack(Snack createSnack);
+
+//  Integer id,
+//  String name,
+//  boolean containsShrimps,
+//  boolean containsCrabs,
+//  boolean containsWheat,
+//  boolean containsBuckwheat,
+//  boolean containsEggs,
+//  boolean containsMilk,
+//  boolean containsPeanuts,
+//  Date updatedAt
 }
